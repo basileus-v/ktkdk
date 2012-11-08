209adb5e24af32cfe11588524458b37b71ad6226
Vassili <vassili@latitude.(none)>
2012-11-08 10:48:06 +0200

    footer with dynamic date, translation files added to repo

--- footer.php
+++ footer.php
@@ -8,17 +8,9 @@
 
 </div><!-- #main -->
 </div><! -- #wrapper -->
-<?php
-  /* A sidebar in the footer? Yep. You can can customize
-  * your footer with three columns of widgets.
-  */
-  if (!is_404()) {
-    // get_sidebar( 'footer' );
-  }
-?>
 <div id="footer" >
   <div class="footertxt" >
-    Kultuuriteaduste ja kunstide doktorikool, aadress: Rävala pst 16, Tallinn 10143. Üldkoordinaator Aleksandra
+    &copy; 2010-<?php echo date('Y');?> Kultuuriteaduste ja kunstide doktorikool, aadress: Rävala pst 16, Tallinn 10143. Üldkoordinaator Aleksandra
     Dolgopolova, <a href="mailto:ktkdk@ema.edu.ee" >ktkdk@ema.edu.ee</a >
   </div >
 </div > <!-- #footer -->
